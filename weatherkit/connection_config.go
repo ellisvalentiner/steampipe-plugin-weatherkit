1c719b4110eebcee869f29d9f2fa1c7ec73c08dc
Ellis Valentiner <ellisvalentiner@users.noreply.github.com>
2022-10-13 09:26:52 -0400

    chore(deps): upgrade steampipe plugin sdk to v4 (#24)
    * chore(deps): upgrade steampipe plugin sdk to v4
    
    * chore: lint
    
    * chore(ci): fix lint workflow

--- weatherkit/connection_config.go
+++ weatherkit/connection_config.go
@@ -1,8 +1,8 @@
 package weatherkit
 
 import (
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/schema"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/schema"
 )
 
 type weatherKitConfig struct {
